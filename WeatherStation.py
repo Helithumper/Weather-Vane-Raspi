8415fa30186f1e1569506c3eed635b9a90db4c1f
Peyton Duncan <peyton.duncan45@gmail.com>
2016-03-07 22:49:10 -0500

    added Database interaction, im tired now :)

--- WeatherStation.py
+++ WeatherStation.py
@@ -13,10 +13,20 @@ secondsCursor = time.time()
 def main():
     print(strftime("%a, %d %b %Y %H:%M:%S",gmtime()))
     while 1:
-        if(time.time()-secondsCursor==5):
+        if(time.time()-secondsCursor==20):
+            db = MySQLdb.connect("localhost","monitor","password","temps")
+            curs = db.cursor()
+
             print("5 Seconds have passed")
             print(strftime("%a, %d %b %Y %H:%M:%S",gmtime()))
+            with db:
+                curs.execute ("""INSERT INTO weather
+                        values(CURRENT_DATE(),NOW(),0,0)""")
+            curs.execute ("SELECT * FROM weather")
 
+            for reading in curs.fetchall():
+                print str(reading[0])+"	"+str(reading[1])+" 	"+"    " + reading[2]+"  	"+str(reading[3])
+            db.close();
 def index():
     return strftime("%a, %d %b %Y %H:%M:%S",gmtime())
 
@@ -24,3 +34,4 @@ if _name_ == '_main_':
     app.run(debug=True,host='0.0.0.0')
 
 main();
+db.close();
