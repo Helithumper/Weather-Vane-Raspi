02c82f8727ff4d94d6599d08b028652e6eeebcd4
Peyton Duncan <peyton.duncan45@gmail.com>
2016-03-08 23:01:19 -0500

    Syntax Fix

--- WeatherStation.py
+++ WeatherStation.py
@@ -1,7 +1,7 @@
 import MySQLdb
 from time import *
-from flask import Flask
-app = Flask(__name__);
+#from flask import Flask
+#app = Flask(__name__);
 
 
 
@@ -14,26 +14,26 @@ def main():
     print(strftime("%a, %d %b %Y %H:%M:%S",gmtime()))
     while 1:
         if i%5==0:
-            db = MySQLdb.connect("localhost","monitor","password","temps")
+            db = MySQLdb.connect(host='localhost',user='monitor',passwd='password',db='weather');
             curs = db.cursor()
 
             print("5 Seconds have passed")
             print(strftime("%a, %d %b %Y %H:%M:%S",gmtime()))
             with db:
-                curs.execute ("""INSERT INTO weather
+                curs.execute ("""INSERT INTO weatherdata
                         values(CURRENT_DATE(),NOW(),0,0)""")
-            curs.execute ("SELECT * FROM weather")
+            curs.execute ("SELECT * FROM weatherdata")
 
             for reading in curs.fetchall():
                 print str(reading[0])+"	"+str(reading[1])+" 	"+"    " + reading[2]+"  	"+str(reading[3])
             db.close();
     i=i+1
-@app.route('/')
-def index():
-    return strftime("%a, %d %b %Y %H:%M:%S",gmtime())
-
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+#@app.route('/')
+#def index():
+#    return strftime("%a, %d %b %Y %H:%M:%S",gmtime())
+#
+#if __name__ == '__main__':
+#    app.run(debug=True,host='0.0.0.0')
 
 main();
 db.close();
