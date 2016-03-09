48923d5c63212921827b495cf8fa6f4fccf4c5c1
Peyton Duncan <peyton.duncan45@gmail.com>
2016-03-08 23:09:22 -0500

    Syntax Fix

--- WeatherStation.py
+++ WeatherStation.py
@@ -1,5 +1,6 @@
 import MySQLdb
 from time import *
+import threading
 #from flask import Flask
 #app = Flask(__name__);
 
@@ -30,6 +31,25 @@ def main():
                 print str(reading[0])+"	"+str(reading[1])+"     " + str(reading[2])+"  	"+str(reading[3])
             db.close();
     j=j+1
+
+def loopedFunction():
+    threading.Timer(2.0, loopedFunction).start()
+    j=0
+    if j%5==0:
+        db = MySQLdb.connect(host='localhost',user='monitor',passwd='password',db='weather');
+        curs = db.cursor()
+
+        print("5 Seconds have passed")
+        print(strftime("%a, %d %b %Y %H:%M:%S",gmtime()))
+        with db:
+            curs.execute ("""INSERT INTO weatherdata
+                    values(CURRENT_DATE(),NOW(),0,0)""")
+        curs.execute ("SELECT * FROM weatherdata")
+
+        for reading in curs.fetchall():
+            print str(reading[0])+"	"+str(reading[1])+"     " + str(reading[2])+"  	"+str(reading[3])
+        db.close();
+        j=j+1
 #@app.route('/')
 #def index():
 #    return strftime("%a, %d %b %Y %H:%M:%S",gmtime())
@@ -37,5 +57,5 @@ def main():
 #if __name__ == '__main__':
 #    app.run(debug=True,host='0.0.0.0')
 
-main();
+loopedFunction()
 db.close();
