5f49774bbbda8a1d74e103a1b3a5ab8ce8d622eb
Peyton Duncan <peyton.duncan45@gmail.com>
2016-04-28 09:18:28 -0400

    Added Pressure to WeatherVainMain

--- WeatherStation.py
+++ WeatherStation.py
@@ -33,6 +33,29 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 GPIO.setup(17,GPIO.OUT)
 
+ADDR = 0x60
+CTRL_REG1 = 0x26
+PT_DATA_CFG = 0x13
+
+who_am_i = bus.read_byte_data(ADDR, 0x0C)
+print hex(who_am_i)
+if who_am_i != 0xc4:
+    print "Device not active."
+    exit(1)
+
+setting = bus.read_byte_data(ADDR, CTRL_REG1)
+newSetting = setting | 0x38
+bus.write_byte_data(ADDR, CTRL_REG1, newSetting)
+
+# Enable event flags
+bus.write_byte_data(ADDR, PT_DATA_CFG, 0x07)
+
+# Toggel One Shot
+setting = bus.read_byte_data(ADDR, CTRL_REG1)
+if (setting & 0x02) == 0:
+    bus.write_byte_data(ADDR, CTRL_REG1, (setting | 0x02))
+
+
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         if ((adcnum > 7) or (adcnum < 0)):
                 return -1
@@ -83,9 +106,30 @@ def getWindSpeed():
     speed = translate(anemometer, 0, 256, 0, 33);
     return speed;
 def getPressure():
-    pas = sensor.read_pressure();
-    inches = pas * 0.0002953;
-    return inches;
+    status = bus.read_byte_data(ADDR,0x00)
+    while (status & 0x08) == 0:
+        #print bin(status)
+        status = bus.read_byte_data(ADDR,0x00)
+    
+    p_data = bus.read_i2c_block_data(ADDR,0x01,3)
+    t_data = bus.read_i2c_block_data(ADDR,0x04,2)
+    status = bus.read_byte_data(ADDR,0x00)
+
+
+    p_msb = p_data[0]
+    p_csb = p_data[1]
+    p_lsb = p_data[2]
+
+
+    pressure = (p_msb << 10) | (p_csb << 2) | (p_lsb >> 6)
+    p_decimal = ((p_lsb & 0x30) >> 4)/4.0
+
+
+
+    #print "Pressure and Temperature at "+time.strftime('%m/%d/%Y %H:%M:%S%z')
+    return str(pressure+p_decimal);
+    #print str(celsius)+deg+"C"
+    #print str(fahrenheit)+deg+"F"
 def getLightSensor():
     adc_value = abs(readadc(1, SPICLK, SPIMOSI, SPIMISO, SPICS))
     Percent = translate(adc_value, 0, 1024, 0, 100)
@@ -112,7 +156,7 @@ def loopedFunction():
         a = getTemp()# getTemp()
         b= getWindSpeed();
         c = getLightSensor();
-        #d = 0#lightLevel()
+        d = getPressure();
         query = """INSERT INTO weatherdata (tdate,ttime,temp,windspeed,sunlight) values(CURRENT_DATE(),NOW(),{},{},{})""".format(a,b,c)
         curs.execute (query)
     curs.execute ("SELECT * FROM weatherdata ORDER BY tdate DESC,ttime DESC LIMIT 1")
