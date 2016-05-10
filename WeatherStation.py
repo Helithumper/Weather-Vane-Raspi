609995982ac1af162a6e65ca4a31746915372ed8
Peyton Duncan <peyton.duncan45@gmail.com>
2016-05-10 13:08:45 -0400

    Fixing Multithreading

--- WeatherStation.py
+++ WeatherStation.py
@@ -171,7 +171,20 @@ def loopedFunction():
 
 def baroFunction():
     threading.Timer(10,baroFunction).start();
+    db = MySQLdb.connect(host='45.55.180.111',user='peyton',passwd='password',db='weather');
+    db.ping(True);
+    curs = db.cursor()
+
+    with db:
+        a = getPressure()
+        query = """INSERT INTO barometer (tdate,ttime,pressure) values(CURRENT_DATE(),NOW(),{})""".format(a)
+        curs.execute (query)
+    curs.execute ("SELECT * FROM barometer ORDER BY tdate DESC,ttime DESC LIMIT 1")
+
+    for reading in curs.fetchall():
+        print str(reading[0])+"|"+str(reading[1])+"|" + str(reading[2])
     print getPressure();
+
 loopedFunction()
 baroFunction()
 
