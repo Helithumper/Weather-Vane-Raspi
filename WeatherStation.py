c7c92f4c9acbb1f3f8ef0e4fd954f299a9f7c425
Peyton Duncan <peyton.duncan45@gmail.com>
2016-05-13 14:32:35 -0400

    Fixing Baro

--- WeatherStation.py
+++ WeatherStation.py
@@ -106,16 +106,17 @@ def getWindSpeed():
     return speed;
 def getPressure():
     # I2C Constants
+    # I2C Constants
     ADDR = 0x60
     CTRL_REG1 = 0x26
     PT_DATA_CFG = 0x13
-    bus = smbus.SMBus(1)
+    bus = SMBus(1)
 
     who_am_i = bus.read_byte_data(ADDR, 0x0C)
-    #print hex(who_am_i)
+    print hex(who_am_i)
     if who_am_i != 0xc4:
         print "Device not active."
-        #exit(1)
+        exit(1)
 
     # Set oversample rate to 128
     setting = bus.read_byte_data(ADDR, CTRL_REG1)
@@ -124,28 +125,39 @@ def getPressure():
 
     # Enable event flags
     bus.write_byte_data(ADDR, PT_DATA_CFG, 0x07)
-    #status = bus.read_byte_data(ADDR,0x00)
-    #status = bus.read_byte_data(ADDR,0x00)
 
+    # Toggel One Shot
+    setting = bus.read_byte_data(ADDR, CTRL_REG1)
+    if (setting & 0x02) == 0:
+        bus.write_byte_data(ADDR, CTRL_REG1, (setting | 0x02))
 
+    # Read sensor data
+    print "Waiting for data..."
+    while (status & 0x08) == 0:
+        status = bus.read_byte_data(ADDR,0x00)
+        #time.sleep(0.5)
+
+    print "Reading sensor data..."
     p_data = bus.read_i2c_block_data(ADDR,0x01,3)
     t_data = bus.read_i2c_block_data(ADDR,0x04,2)
     status = bus.read_byte_data(ADDR,0x00)
-
+    print "status: "+bin(status)
 
     p_msb = p_data[0]
     p_csb = p_data[1]
     p_lsb = p_data[2]
-
+    t_msb = t_data[0]
+    t_lsb = t_data[1]
 
     pressure = (p_msb << 10) | (p_csb << 2) | (p_lsb >> 6)
     p_decimal = ((p_lsb & 0x30) >> 4)/4.0
-    #print p_decimal;
 
-
+    celsius = t_msb + (t_lsb >> 4)/16.0
+    fahrenheit = (celsius * 9)/5 + 32
 
     #print "Pressure and Temperature at "+time.strftime('%m/%d/%Y %H:%M:%S%z')
-    return pressure+p_decimal;
+    return pressure+p_decimal
+
     #print str(celsius)+deg+"C"
     #print str(fahrenheit)+deg+"F"
 def getLightSensor():
