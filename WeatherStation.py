902fd95a37a0e8549d35fad23b71951c2ff34908
Peyton Duncan <peyton.duncan45@gmail.com>
2016-05-19 11:17:34 -0400

    fixing wind

--- WeatherStation.py
+++ WeatherStation.py
@@ -2,7 +2,7 @@ import MySQLdb
 from time import *
 from random import *
 import threading
-import smbus
+from smbus import SMBus
 import RPi.GPIO as GPIO
 #import Adafruit_BMP.BMP085 as BMP085
 
@@ -16,7 +16,7 @@ GPIO.setmode(GPIO.BCM)
 db = MySQLdb.connect(host='45.55.180.111',user='peyton',passwd='password',db='weather');
 curs = db.cursor();
 
-bus = smbus.SMBus(1)
+bus = SMBus(1)
 address = 0x48
 
 SPICLK = 18
@@ -123,11 +123,41 @@ def getWindSpeed():
     return abs(speed);
 def getPressure():
     # I2C Constants
-    # I2C Constants
     ADDR = 0x60
     CTRL_REG1 = 0x26
     PT_DATA_CFG = 0x13
     bus = SMBus(1)
+    
+    who_am_i = bus.read_byte_data(ADDR, 0x0C)
+    print hex(who_am_i)
+    if who_am_i != 0xc4:
+        print "Device not active."
+        exit(1)
+
+    # Set oversample rate to 128
+    setting = bus.read_byte_data(ADDR, CTRL_REG1)
+    newSetting = setting | 0x38
+    bus.write_byte_data(ADDR, CTRL_REG1, newSetting)
+
+    # Enable event flags
+    bus.write_byte_data(ADDR, PT_DATA_CFG, 0x07)
+
+    # Toggel One Shot
+    setting = bus.read_byte_data(ADDR, CTRL_REG1)
+    if (setting & 0x02) == 0:
+        bus.write_byte_data(ADDR, CTRL_REG1, (setting | 0x02))
+
+    # Read sensor data
+    print "Waiting for data..."
+    while (status & 0x08) == 0:
+        status = bus.read_byte_data(ADDR,0x00)
+        #time.sleep(0.5)
+
+    print "Reading sensor data..."
+    p_data = bus.read_i2c_block_data(ADDR,0x01,3)
+    t_data = bus.read_i2c_block_data(ADDR,0x04,2)
+    status = bus.read_byte_data(ADDR,0x00)
+    print "status: "+bin(status)
 
     p_msb = p_data[0]
     p_csb = p_data[1]
@@ -136,11 +166,8 @@ def getPressure():
     pressure = (p_msb << 10) | (p_csb << 2) | (p_lsb >> 6)
     p_decimal = ((p_lsb & 0x30) >> 4)/4.0
 
-    #print "Pressure and Temperature at "+time.strftime('%m/%d/%Y %H:%M:%S%z')
-    return pressure+p_decimal
 
-    #print str(celsius)+deg+"C"
-    #print str(fahrenheit)+deg+"F"
+    return pressure+p_decimal
 def getLightSensor():
     adc_value = abs(readadc(1, SPICLK, SPIMOSI, SPIMISO, SPICS))
     Percent = translate(adc_value, 0, 1024, 0, 100)
