589957c59fbc460c869467eca2bff21fc0a0b796
Helithumper <peyton.duncan45@gmail.com>
2016-05-29 13:25:25 -0400

    Fixing Incrementing Issue

--- WeatherStation.py
+++ WeatherStation.py
@@ -176,7 +176,7 @@ def getLightSensor():
 
 def loopedFunction():
 
-    threading.Timer(2.0, loopedFunction).start()
+    threading.Timer(5.0, loopedFunction).start()
     GPIO.output(17,False);
 
     db = MySQLdb.connect(host='45.55.180.111',user='peyton',passwd='password',db='weather');
